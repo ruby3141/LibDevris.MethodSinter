18dca7bc364870e3539b8d3166c24c158781a9d7
CDnX <ruby3141@gmail.com>
2024-02-01 11:43:34 +0900

    add SinteredMethodGenerator and target attributes.

---
+++ Devris.LibDevris.MethodSinter/MethodFragmentAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Devris.LibDevris.MethodSinter
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public sealed class MethodFragmentAttribute : Attribute
+    {
+        public string Identifier { get; private set; }
+
+        public MethodFragmentAttribute(string identifier)
+        {
+            Identifier = identifier;
+        }
+    }
+}
---
+++ Devris.LibDevris.MethodSinter/SinteredMethodAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Devris.LibDevris.MethodSinter
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public sealed class SinteredMethodAttribute : Attribute
+    {
+        public string Identifier { get; private set; }
+
+        public SinteredMethodAttribute(string identifier)
+        {
+            Identifier = identifier;
+        }
+    }
+}
