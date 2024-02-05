83799c7c5b9136b1884dae5e6aeb8f04e5666c1b
CDnX <ruby3141@gmail.com>
2024-02-05 11:05:52 +0900

    refactor SinteredMethodGenerator
    From guide on https://andrewlock.net/creating-a-source-generator-part-9-avoiding-performance-pitfalls-in-incremental-generators/#7-consider-using-registerimplementationsourceoutput-instead-of-registersourceoutput,
    > Don't use `*Syntax` or `ISymbol` instances in your pipeline
    
    This change makes IncrementalProvider to render everything to struct
    before RegisterSourceOutput(), expecting better performance with caching.

--- Devris.LibDevris.MethodSinter.Generator/SinteredMethodGenerator.cs
+++ Devris.LibDevris.MethodSinter.Generator/SinteredMethodGenerator.cs
@@ -1,7 +1,6 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -16,59 +15,122 @@ namespace Devris.LibDevris.MethodSinter.Generator
             var sinteredMethodProvider =
                 context.SyntaxProvider.ForAttributeWithMetadataName("Devris.LibDevris.MethodSinter.SinteredMethodAttribute",
                     predicate: static (s, _) => s is MethodDeclarationSyntax mds && mds.Modifiers.Any(m => m.IsKind(SyntaxKind.PartialKeyword)),
-                    transform: static (gac, _) => (
-                        Identifier: (string)gac.Attributes.Single().ConstructorArguments.Single().Value,
-                        TargetNode: (MethodDeclarationSyntax)gac.TargetNode));
+                    transform: static (gac, _) => new SinteredMethodData
+                    {
+                        AttributeIdentifier = (string)gac.Attributes.Single().ConstructorArguments.Single().Value,
+                        Ancestor = new SinteredMethodData.AncestorData
+                        {
+                            Usings = ((MethodDeclarationSyntax)gac.TargetNode).FirstAncestorOrSelf<CompilationUnitSyntax>().Usings
+                                .Select(u => u.ToString()).Where(s => string.IsNullOrWhiteSpace(s) == false)
+                                .ToImmutableArray(),
+                            NamespaceName = ((MethodDeclarationSyntax)gac.TargetNode).FirstAncestorOrSelf<NamespaceDeclarationSyntax>().Name.ToString(),
+                            ClassName = ((MethodDeclarationSyntax)gac.TargetNode).FirstAncestorOrSelf<ClassDeclarationSyntax>().Identifier.Text,
+                        },
+                        Method = new SinteredMethodData.MethodData
+                        {
+                            Modifiers = ((MethodDeclarationSyntax)gac.TargetNode).Modifiers.ToString(),
+                            MethodName = ((MethodDeclarationSyntax)gac.TargetNode).Identifier.Text,
+                            Parameters = ((MethodDeclarationSyntax)gac.TargetNode).ParameterList.Parameters
+                                .Select(p => new SinteredMethodData.MethodData.ParameterData
+                                {
+                                    Attributes = p.AttributeLists.ToString(),
+                                    Modifier = p.Modifiers.ToString(),
+                                    Type = p.Type.ToString(),
+                                    ParameterName = p.Identifier.Text
+                                }).ToImmutableArray(),
+                        },
+                    });
 
             var methodFragmentProvider =
-                context.SyntaxProvider.ForAttributeWithMetadataName("Devris.LibDevris.MethodSinter.MethodFragmentAttribute",
-                    predicate: static (s, _) => s is MethodDeclarationSyntax,
-                    transform: static (gac, _) => (
-                        Identifier: (string)gac.Attributes.Single().ConstructorArguments.Single().Value,
-                        TargetNode: (MethodDeclarationSyntax)gac.TargetNode))
+                context.SyntaxProvider
+                    .ForAttributeWithMetadataName("Devris.LibDevris.MethodSinter.MethodFragmentAttribute",
+                        predicate: static (s, _) => s is MethodDeclarationSyntax,
+                        transform: static (gac, _) => (
+                            AttributeIdentifier: (string)gac.Attributes.Single().ConstructorArguments.Single().Value,
+                            MethodName: ((MethodDeclarationSyntax)gac.TargetNode).Identifier.Text))
                     .Collect();
 
-            var targetProvider = sinteredMethodProvider.Combine(methodFragmentProvider);
+            var targetProvider = sinteredMethodProvider.Combine(methodFragmentProvider).Select((ps, _) =>
+            {
+                ps.Left.FragmentNames = ps.Right
+                    .Where(t => t.AttributeIdentifier == ps.Left.AttributeIdentifier)
+                    .Select(t => t.MethodName)
+                    .ToImmutableArray();
+
+                return ps.Left;
+            });
 
             context.RegisterSourceOutput(targetProvider, GenerateSintered);
         }
 
-        private void GenerateSintered(
-            SourceProductionContext context,
-            ((string Identifier, MethodDeclarationSyntax TargetNode) Method, ImmutableArray<(string Identifier, MethodDeclarationSyntax TargetNode)> Fragments) source)
+        private void GenerateSintered(SourceProductionContext context, SinteredMethodData source)
         {
-            var usings = source.Method.TargetNode.FirstAncestorOrSelf<CompilationUnitSyntax>().Usings
-                .Select(u => u.ToString()).Where(s => string.IsNullOrWhiteSpace(s) == false);
-
-            string namespaceName = source.Method.TargetNode.FirstAncestorOrSelf<NamespaceDeclarationSyntax>().Name.ToString();
-            string className = source.Method.TargetNode.FirstAncestorOrSelf<ClassDeclarationSyntax>().Identifier.Text;
-            var modifiers = source.Method.TargetNode.Modifiers;
-            string methodName = source.Method.TargetNode.Identifier.Text;
-            var inputParameters = source.Method.TargetNode.ParameterList.Parameters
-                .Select(p => p.Modifiers.ToString() switch
+            var inputParameters = source.Method.Parameters
+                .Select(p => p.Modifier switch
                     {
-                        "params" or "" => p.Identifier.Text,
-                        "ref readonly" => $"in {p.Identifier.Text}",
-                        var mod => $"{mod} {p.Identifier.Text}"
+                        "params" or "" => p.ParameterName,
+                        "ref readonly" => $"in {p.ParameterName}",
+                        var mod => $"{mod} {p.ParameterName}"
                     });
 
-            context.AddSource($"{className}.{source.Method.Identifier}.g.cs",
+            context.AddSource($"{source.Ancestor.ClassName}.{source.AttributeIdentifier}.g.cs",
 $@"// <auto-generated />
-{string.Join("\r\n", usings)}
+{string.Join("\r\n", source.Ancestor.Usings)}
 
-namespace {namespaceName}
+namespace {source.Ancestor.NamespaceName}
 {{
-    partial class {className}
+    partial class {source.Ancestor.ClassName}
     {{
-        {modifiers} void {methodName}{string.Join(", ", source.Method.TargetNode.ParameterList)}
+        {source.Method.Modifiers} void {source.Method.MethodName}({string.Join(", ", source.Method.Parameters)})
         {{
-            {string.Join("\r\n            ", source.Fragments
-                .Where(f => f.Identifier == source.Method.Identifier)
-                .Select(f => $"{f.TargetNode.Identifier.Text}({string.Join(", ", inputParameters)});"))}
+            {string.Join("\r\n            ", source.FragmentNames.Select(f => $"{f}({string.Join(",", inputParameters)});"))}
         }}
     }}
 }}
 ");
         }
+
+        struct SinteredMethodData
+        {
+            public string AttributeIdentifier { get; set; }
+
+            public AncestorData Ancestor { get; set; }
+            public struct AncestorData
+            {
+                public IEnumerable<string> Usings { get; set; }
+
+                public string NamespaceName { get; set; }
+
+                public string ClassName { get; set; }
+            }
+
+            public MethodData Method { get; set; }
+            public struct MethodData
+            {
+                public string Modifiers { get; set; }
+
+                public string MethodName { get; set; }
+
+                public IEnumerable<ParameterData> Parameters { get; set; }
+                public struct ParameterData
+                {
+                    public string Attributes { get; set; }
+
+                    public string Modifier { get; set; }
+
+                    public string Type { get; set; }
+
+                    public string ParameterName { get; set; }
+
+                    public override string ToString()
+                    {
+                        return string.Join(" ", new[] { Attributes, Modifier, Type, ParameterName }.Where(s => string.IsNullOrWhiteSpace(s) == false));
+                    }
+                }
+            }
+
+            public IEnumerable<string> FragmentNames { get; set; }
+        }
     }
 }
+
